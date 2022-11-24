2a3ad6c86780a3ea75cfcf46e267e8037a8ff7a9
JulieKodehode <julie.kodehode@gmail.com>
2022-11-24 11:07:51 +0100

    Updated App.js with basic idea

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
 function App() {
 	return (
-		// Routes and route here
-		<main>
-			<h1>Velkommen</h1>
-		</main>
+		// Routes and route here? OR ONE BIG SCROLL PAGE
+		<div>
+			{/* Header */}
+			<header>
+				<nav>
+					<ul>
+						<li>Hjem</li>
+						<li>Om meg</li>
+						<li>Prosjekter</li>
+						<li>Kontakt</li>
+					</ul>
+				</nav>
+			</header>
+
+			{/* Main */}
+			<main>
+				<div>
+					<h1>Velkommen</h1>
+				</div>
+
+				<div>
+					<h3>Om meg</h3>
+					<p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
+				</div>
+
+				<div>
+					<img
+						style={{ height: "100px", widht: "100px" }}
+						src="https://static.scientificamerican.com/sciam/cache/file/32665E6F-8D90-4567-9769D59E11DB7F26_source.jpg?w=590&h=800&7E4B4CAD-CAE1-4726-93D6A160C2B068B2"
+						alt="cat? me? what?"
+					/>
+				</div>
+
+				<div>
+					<h3>Prosjekter</h3>
+					<p style={{ height: "100px", width: "100px", border: "5px solid black" }}>Prosjekt A</p>
+					<p style={{ height: "100px", width: "100px", border: "5px solid black" }}>Prosjekt B</p>
+					<p style={{ height: "100px", width: "100px", border: "5px solid black" }}>Prosjekt C</p>
+				</div>
+
+				<div>
+					<h3>Kontakt</h3>
+					<p>Github link xyz</p>
+					<p>Gmail xyz</p>
+				</div>
+			</main>
+
+			{/* Footer */}
+			<footer>
+				<h4>BYE</h4>
+			</footer>
+		</div>
 	);
 }
 
