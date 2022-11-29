e88a1523e0821b72a16307b9a975d1889ea4d40d
JulieKodehode <julie.kodehode@gmail.com>
2022-11-29 12:44:25 +0100

    Ups

---
+++ src/routes/LandingPage.jsx
@@ -0,0 +1,26 @@
+// Libraries
+import { Outlet } from "react-router-dom";
+
+// Styles
+import "../styles/basic.css";
+
+// Components
+import NavBar from "../components/CaseSensetivity/NavBar";
+import Footer from "../components/Footer";
+
+/**
+ * This is the landing page of the project
+ */
+const LandingPage = () => {
+	return (
+		<>
+			<NavBar />
+
+			<Outlet />
+
+			<Footer />
+		</>
+	);
+};
+
+export default LandingPage;
