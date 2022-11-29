8978efa0c6e28db280834c4a2ca614a3ffc90b09
JulieKodehode <julie.kodehode@gmail.com>
2022-11-29 14:49:13 +0100

    Footer css updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,14 +3,20 @@
 // CSS
 import styles from "./layout.module.css";
 
+// Components
+import Waves from "../Waves/Waves";
+
 /**
  * Main footer feature
  */
 const Footer = () => {
 	return (
-		<footer className={styles.footer}>
-			<h3 className={styles.footerH3}>Julie Hauge 2022</h3>
-		</footer>
+		<>
+			<Waves />
+			<footer className={styles.footer}>
+				<h3 className={styles.footerH3}>Julie Hauge 2022</h3>
+			</footer>
+		</>
 	);
 };
 
