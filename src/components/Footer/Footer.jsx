ad7c14d8a7faccd841125d88662a4fa25905cc69
JulieKodehode <julie.kodehode@gmail.com>
2022-12-05 12:30:06 +0100

    Removal of unfunctional wave

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,6 @@
 import styles from "./layout.module.css";
 
 // Components
-// import Waves from "../Waves/Waves";
 
 /**
  * Main footer feature
@@ -12,7 +11,6 @@ import styles from "./layout.module.css";
 const Footer = () => {
 	return (
 		<footer className={styles.footer}>
-			{/* <Waves /> */}
 			<h3 className={styles.footerH3}>Julie Hauge 2022</h3>
 		</footer>
 	);
--- src/components/Waves/readme.md
+++
@@ -1,7 +0,0 @@
-## Waves
-
-I want to feature waves
-
-### No original html, only css
-
-Waves is put on hold until further notice 01.12.22
